37a8cfae7954b5e2d563d0ecaa2469a892fb93ce
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-03-16 10:43:49 +0700

    BaiTap

--- src/Point2dAnd3d/Main.java
+++ src/Point2dAnd3d/Main.java
@@ -1,4 +1,23 @@
 package Point2dAnd3d;
 
 public class Main {
+    public static void main(String[] args) {
+        printTestRange("POINT2D");
+        Point2d pnt = new Point2d();
+        System.out.println(pnt);
+        Point2d pnt1 = new Point2d(10, 20);
+        System.out.println(pnt1);
+        printTestRange("POINT3D");
+        Point3d pnt2 = new Point3d();
+        System.out.println(pnt2);
+        Point3d pnt3 = new Point3d(10,20,30);
+        System.out.println(pnt3);
+
+
+    }
+    public static void printTestRange(String value){
+        System.out.println();
+        System.out.println("------------- " + value + " -------------");
+        System.out.println();
+    }
 }
--- src/Point2dAnd3d/Point3d.java
+++ src/Point2dAnd3d/Point3d.java
@@ -1,4 +1,50 @@
 package Point2dAnd3d;
 
 public class Point3d {
+    private float z;
+
+    public Point3d(float x, float y, float z) {
+        super();
+        this.z = z;
+    }
+
+    public Point3d() {
+        super();
+        this.z = 50;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        setXY(x,y);
+        this.z = z;
+    }
+
+    private void setXY(float x, float y) {
+    }
+
+    public float[] getXYZ(){
+        return new float[] {getX(),getY(),z};
+    }
+
+    private float getY() {
+        return 0;
+    }
+
+    private float getX() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "z=" + z +
+                "} " + super.toString();
+    }
 }
